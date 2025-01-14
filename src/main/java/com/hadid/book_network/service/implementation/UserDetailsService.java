23fd3170b73c92cdced0b3b78b3d1bc9d044ce1e
Hadid Hardiansyah <hadidhardiansyah@gmail.com>
2025-01-14 14:54:43 +0700

    Implement user details service

---
+++ src/main/java/com/hadid/book_network/service/implementation/UserDetailsService.java
@@ -0,0 +1,22 @@
+package com.hadid.book_network.service.implementation;
+
+import com.hadid.book_network.repository.IUserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+    private final IUserRepository userRepository
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
