cdee8020ea2c1e3ad0339fb14388dd77f89e8088
Hadid Hardiansyah <hadidhardiansyah@gmail.com>
2025-01-14 08:44:26 +0700

    Initial commit

---
+++ src/main/java/com/hadid/book_network/RestfulApiBookNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.hadid.book_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiBookNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiBookNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hadid/book_network/RestfulApiBookNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadid.book_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiBookNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
